9c88561ae9e70f8c23819d91794c68c38bc1c188
Vikas Yadav <yadavvikas859@gmail.com>
2021-08-13 02:20:17 +0530

    crispy forms added

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'crispy_forms',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -125,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK ='bootstrap4'
