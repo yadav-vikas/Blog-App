0418ed66743e4b00c05582e172be68af4d1d5ad3
Vikas Yadav <yadavvikas859@gmail.com>
2021-08-12 21:43:35 +0530

    database added

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User # one to many relationship
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    #auto_now_add = True --> time at when post was created
+    #auto_now =True --> time at when the post was updated
+    #default=timezone.now a function Returns a datetime that represents the current point in time. 
+    # NOTE: we are note executing the timezone.now() function for any new values since we want only defualt value
+
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    # on_delete to delete content related to the author
+    # NOTE: when a author deleted all posted are deleted related to that author
+
+    def __str__(self) -> str:
+        return self.title
