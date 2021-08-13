f8b585a6afaff3b9d11b781d94ccd3edaf956965
Vikas Yadav <yadavvikas859@gmail.com>
2021-08-13 17:12:37 +0530

    lastest post on top

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
 from . import views
+from .views import PostListView
 
 urlpatterns = [
-    path('',views.home,name='blog-home'),
+    path('',PostListView.as_view(),name='blog-home'), #url will be<app>/<model>_<viewtype(eg. listview)>.html
     path('about/',views.about,name='blog-about'),
     
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import  Post
+from django.views.generic import ListView
 
 
 def home(request):
@@ -8,6 +9,22 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    """Home page showing all the posts, so we use list views to show all posts
+    Types of Class Based views :
+    class based views ---------
+	list views   (list of subs on youtube)
+	detail views (click on one of subs on list)	
+	create views (create video)
+	update views (update video)
+	delete views (delete video)
+    Args:
+        ListView ([type]): [description]
+    """
+    model = Post
+    template_name = 'blog/home.html' #app/model_viewtype.html
+    context_object_name = 'posts' # calling each posts as views
+    ordering = ['-date_posted']  # order of post -date_posted will be descending and date_posted will be ascending('-'sign)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
