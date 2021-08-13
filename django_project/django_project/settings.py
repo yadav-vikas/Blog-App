be50d9ed27aa66b21785197de6f0fe6804d05aa1
Vikas Yadav <yadavvikas859@gmail.com>
2021-08-14 03:21:13 +0530

    added reset password

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -139,3 +139,12 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 #undefiend or unaccessible pages link will be redirected to login page
 LOGIN_URL = 'login'
+
+#setting up email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
